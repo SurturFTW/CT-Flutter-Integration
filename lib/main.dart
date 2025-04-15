d17a95eca9569d156f051bd1fd6ba7e8f101bd55
SurturFTW <pushkarsane07@gmail.com>
2025-04-15 10:30:18 +0530

    /*Minor changes*/

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,6 @@ Future<void> _sendLocationToCleverTap() async {
     locationSettings: LocationSettings(accuracy: LocationAccuracy.high),
   );
   CleverTapPlugin.setLocation(position.latitude, position.longitude);
-  CleverTapPlugin.deleteNotificationChannel("fluttertest");
 }
 
 class MyApp extends StatelessWidget {
@@ -46,7 +45,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'CT Flutter Integration'),
     );
   }
 }
@@ -88,17 +87,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
